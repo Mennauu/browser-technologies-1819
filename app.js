72245456a1d18028a12697cc0f163024cf3ccdb6
Menno <mennodevries1@msn.com>
2019-03-25 12:00:48 +0100

    port change to 3000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const shrinkRay = require('shrink-ray-current')
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 8000
+const port = 3000
 
 // Disable x-powered-by header
 app.disable('x-powered-by')
